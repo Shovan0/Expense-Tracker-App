d241f772998ed8a63f9ce0635c6a29392597ed14
Shovan0 <shovannath4039@gmail.com>
2025-09-18 05:36:35 +0530

    change the summary controller

--- backend/src/controllers/transactionController.js
+++ backend/src/controllers/transactionController.js
@@ -4,10 +4,10 @@ export const sql = neon(process.env.DB_URL);
 
 export const createTransactions = async (req, res) => {
     try {
-        const { title, amount, category, user_id } = req.body;
+        const {title, amount, category, user_id} = req.body;
 
-        if (!title || amount === undefined || !user_id || !category) {
-            return res.status(400).json({ message: "All fields are required!" });
+        if(!title || amount===undefined || !user_id || !category)  {
+            return res.status(400).json({message: "All fields are required!"});
         }
 
         const transactions = await sql`
@@ -26,7 +26,7 @@ export const createTransactions = async (req, res) => {
 
 export const getTransactions = async (req, res) => {
     try {
-        const { userId } = req.params;
+        const {userId} = req.params;
 
         const transactions = await sql`
             SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
@@ -42,22 +42,21 @@ export const getTransactions = async (req, res) => {
 
 export const getSummary = async (req, res) => {
     try {
-        const { userId } = req.params;
+        const {userId} = req.params;
         console.log("userId: ", userId);
 
         const balanceResult = await sql`
             SELECT COALESCE(SUM(amount), 0) as balance FROM transactions
             WHERE user_id = ${userId}
-        `
+        ` 
         const incomeResult = await sql`
             SELECT COALESCE(SUM(amount), 0) as income FROM transactions
             WHERE user_id = ${userId} AND amount > 0
         `
         const expenseResult = await sql`
-            SELECT COALESCE(SUM(-amount), 0) as expenses FROM transactions
+            SELECT COALESCE(SUM(-amount), 0) as expense FROM transactions
             WHERE user_id = ${userId} AND amount < 0
         `
-
         res.status(200).json({
             balance: balanceResult[0].balance,
             income: incomeResult[0].income,
@@ -72,19 +71,19 @@ export const getSummary = async (req, res) => {
 
 export const deleteTransactions = async (req, res) => {
     try {
-        const { id } = req.params;
+        const {id} = req.params;
 
-        if (isNaN(id)) {
-            return res.status(400).json({ message: "Invalid transaction id" })
+        if(isNaN(id))  {
+            return res.status(400).json({message:"Invalid transaction id"})
         }
 
         const result = await sql`
         DELETE FROM transactions WHERE id=${id} RETURNING *
         `
-        if (result.length == 0) {
-            return res.send(404).json({ message: "No transaction found" })
+        if(result.length == 0) {
+            return res.send(404).json({message:"No transaction found"})
         }
-        res.status(200).json({ message: "Deleted successfully" })
+        res.status(200).json({message:"Deleted successfully"})
     } catch (error) {
         console.log("Error deleting transactions")
         res.status(500).json(error);
